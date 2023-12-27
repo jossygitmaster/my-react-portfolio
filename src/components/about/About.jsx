37ce6c8ded44c73f9a87caa68a57f3e87b098b61
Jossy <128414622+jossygitmaster@users.noreply.github.com>
2023-12-27 06:59:59 -0800

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -45,18 +45,81 @@ return (
 </article>
 </div>
 <p>
- once said, "The biggest adventure you can take is to live the life of your dreams". 
-One day I saw in my dreams of being a successful Author so wise, appreciated, and sought-after
- by society, in my early childhood I used to daydream about contributing Infographics for the NatGeo magazine
-  (the wonders of my childhood) while flipping through the pages of NatGeo Megazine I stumbled on in MYC library
-   (youth center in my hometown) this influenced my interest in mixing science with visual communication. During those days I dreamed of my family, friends from school, and acquaintances
-    being proud of me (I felt it). Is it too much to dream? :) 
-    <p></p>
-I woke up amidst that dream at 4 in the morning and jotted them down in my notebook. 
-I grip those exciting dreams firmly. After that blissful moment, I sustained waking 
-up at 4 AM every day to my Miracle Morning(Thanks to Harl Elrod) and painting my dreams
- to glow on the canvas of reality that hung on the bars of faith, with the ink of wisdom
-  and a brush of discipline. Praise be to God, I am on the right path toward being the Majestic Me.
+Amidst the symphony of my formative years, interwoven with the echoes of war and the distant notes of hope, I sought refuge in the ethereal realm of
+imagination. Like a flickering flame dancing in the boundless darkness, I envisioned a path where the cadence of my voice would transcend the ashes 
+of conflict, carrying forth the resilient tales of my noble Tigrayan brethren. Dreams, akin to the mellifluous lyrics of K'naan's "Wavin' Flag," 
+resonated within me, for they are the celestial fuel that ignites the embers of our spirits.
+
+During the tender embrace of those youthful days, as the chain of losses unfurled amidst the relentless ravages of war, I discovered solace and 
+enlightenment within the hallowed pages of the few books that emerged unscathed from the tempest. Amidst the makeshift libraries that sprouted 
+amidst the chaos, a tattered copy of Robert Greene's magnum opus, "The Laws of Human Nature," found its way into my hands. A beacon of wisdom 
+amid the debris, it became an oasis in the desert of uncertainty, nurturing the seeds of my fervent passion to intertwine the sciences with the 
+tapestry of psychology. The profound insights chronicled within those sacred tomes became a guiding light, illuminating the intricate depths of
+human nature and the indelible forces that shape our very existence.
+
+With each loss, each shattered dream, I delved deeper into the labyrinthine corridors of the human psyche, propelled by an insatiable curiosity to 
+unravel the enigmas shrouded beneath the scars. The pages of knowledge became my sanctuary, bestowing upon me wisdom and understanding as I 
+endeavored to fathom the complexities of human behavior and the profound impact it orchestrates upon our lives, our communities, and the collective 
+journey toward healing.
+</p>
+<p>
+In the face of adversity, amidst the ruins and remnants of my homeland, I honed my literary prowess, weaving words that transcended the ephemeral
+and resonated with the profound essence of our war-torn experiences. Through my writings, I aspired to contribute to the collective well-being of 
+my people and humanity at large, offering insights and perspectives that would illuminate paths of healing, self-discovery, and personal growth.
+
+Those losses, though harbingers of anguish, became the crucible that forged the essence of my understanding, birthing within me a profound empathy
+and an acute discernment of the human condition amidst the maelstrom of conflict. It was within the hearts of my resilient Tigrayan kin that I 
+unearthed the transformative power of words—words capable of caressing wounded souls, igniting the flames of change, and instilling fragile seeds of
+hope amidst the desolation of despair.
+
+Amidst the tapestry of my existence, I bear the weight of a profound abandonment, an indelible imprint upon the very fabric of my being. 
+The untimely demise of my beloved mother when I was but a tender seven years old struck a devastating blow to my youthful heart, leaving 
+me adrift in an abyss of grief and longing. In the wake of her passing, the absence of her nurturing presence birthed a void seemingly
+insurmountable.
+
+Compounding the pain of loss, my noble father, burdened by the demands of his vocation as a stalwart truck driver, was often shrouded in the veils
+of absence. The rumble of his engine and the ephemeral sight of his departing truck became poignant symbols of his physical distance, leaving me 
+yearning for the love and guidance that only a father can bestow. As I traversed the tumultuous waters of childhood, I found myself grappling with
+the piercing pangs of abandonment and the yearning for connection.
+</p>
+<p>
+In the depths of my solitude, I sought solace within the ethereal realm of imagination and literature. Books became my steadfast companions, 
+offering solace and a sense of belonging within their sacred pages. Through the power of storytelling, I discovered a sanctuary where I could 
+transcend the harsh realities of my existence and find solace in the lives and adventures of fictional characters. In those fleeting moments,
+I transcended the boundaries of my own circumstances, delving into worlds where the wounds of abandonment found respite, if only momentarily.
+
+Yet, within the recesses of my heart, the ache persisted. The absence of my mother's tender love and the intermittent presence of my noble father 
+reverberated throughout the tapestry of my formative years, shaping my understanding of the human experience. Through the prism of my own abandonment
+, I cultivated a profound empathy for those who bore the weight of similar trials and tribulations, nurturing within me an ardent desire to offer
+solace and understanding to those who felt lost and forsaken, reminding them that they are not alone in their pain.
+
+As I embarked upon the labyrinthine path of self-discovery and understanding, the wisdom gleaned from psychological insights intertwined with the
+resilience of my indomitable Tigrayan heritage, converging with the currents of my own experiences of abandonment. Togetherthey formed the bedrock
+upon which I crafted my writings, infusing each word with authenticity and compassion born from personal struggle. Through my stories, I endeavored
+to give voice to the voiceless, to illuminate the path of healing and resilience, and to inspire others to discover the dormant strength within 
+themselves.
+
+In the face of my own losses and the echoes of war, I discovered that the power of words transcends the confines of time and space. It possesses the
+profound ability to bridge the chasms of abandonment and despair, connecting us in the tapestry of our shared humanity. Like a flag gently swaying
+in the wind, my voice rises above the ashes of conflict, carrying tales of resilience and hope, serving as a poignant reminder that each individual
+possesses the inner strength required to rise above their own trials.
+
+Through the intricate tapestry of my experiences, I have come to realize that our wounds do not define us; rather, it is how we navigate and 
+transcend them that shapes our true essence. The scars we bear are a testament to our resilience and our capacity for growth. They serve as
+reminders of the battles we have fought and the strength we have summoned in the face of adversity.
+</p>
+<p>
+In my writings, I strive to delve into the profound depths of the human psyche, unraveling the intricacies of our emotions and shedding light on the
+transformative power of self-discovery and healing. Through the pages of my stories, I aim to offer solace, inspiration, and a profound sense of
+connection to those who may be grappling with their own wounds, reminding them that they are not alone on their arduous journey.
+
+From the ashes of loss and abandonment, I have emerged with a profound understanding of the indomitable human spirit, an ever-flowing wellspring of 
+compassion, and an unwavering belief in the power of resilience. It is my fervent hope that through my words, I can touch the hearts of others, 
+igniting a spark of hope within them and inspiring them to embrace their own strength and untapped potential.
+
+Ultimately, my purpose as a writer and storyteller is to weave narratives that transcend the boundaries of time and culture, bridging the chasms of
+understanding and empathy. In doing so, I seek to remind us all of the profound truth that resides within each and every one of us—the indomitable 
+spirit that perseveres, the spirit that carries us through the darkest of nights and illuminates the path toward healing and growth.
   <p></p>
 I am sailing on the vast ocean of Literature and Art, traveling the world hunting for the treasure of wisdom and the marvels of beauty.
 </p>
