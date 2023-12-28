b826124be70837bd1b234eed20961d0f0963e24e
Jossy <128414622+jossygitmaster@users.noreply.github.com>
2023-12-28 00:47:36 -0800

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,8 +11,8 @@ const header = () => {
       <h5>Hello I'm</h5>
       <h1>Yosef K.Hailu</h1>
       <h3 className='text-light'>Prolific Writer</h3>
-      <h4 className='text-light'>Graphic Designer<br></br></h4>
-      <h5 className='text-light'>Developer</h5>
+      <h4 className='text-light'>Translator<br></br></h4>
+      <h5 className='text-light'>Bibliophile</h5>
       <TMM/>
       <div className='me'>
                 <img src={me} alt='me'/>
