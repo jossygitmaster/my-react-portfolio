d497699ef79b52ef041e4dd89a3d69e9668c8bf5
Jossy <128414622+jossygitmaster@users.noreply.github.com>
2023-12-28 08:30:36 -0800

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './portfolio.css'
 import image0 from '../../assets/burger.jpg'
-import image1 from '../../assets/pro1.png'
-import image2 from '../../assets/enat1.png'
+//import image1 from '../../assets/pro1.png'
+//import image2 from '../../assets/enat1.png'
 
 import image3 from '../../assets/ቻው ገርነት.jpg'
 
@@ -31,7 +31,7 @@ const portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={image0} alt=''/> 
           </div>
-          <h3>my favorite burger ad, this was developed for a client in US.</h3>
+          <h3>just trying some graphic design burger ad, this was developed for a client in US.</h3>
           <div className= 'portfolio-item-cta'>
           <a href='wwww.upwork.com/me' className='btn' target='_blank'>upwork</a>
           <a href='www.facebook.com/me' className='btn btn-primary' target='_blank'>facebook</a>
